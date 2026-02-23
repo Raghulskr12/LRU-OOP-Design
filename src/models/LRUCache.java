0e13f7e715bf248de4a235b11c2173073176d0b7
Raghulskr12 <kalairaghul70@gmail.com>
2026-02-23 16:14:00 +0530

    [ADD] insertAtHead and DeleteNode functionality added

--- src/models/LRUCache.java
+++ src/models/LRUCache.java
@@ -21,6 +21,38 @@ public class LRUCache {
 
     }
 
+    public void insertAtHead(Node node)
+    {
+        Node n1 = head.next;
+        head.next=node;
+        node.prev=head;
+        node.next=n1;
+        n1.prev=node;
+
+    }
+
+    public void deleteNode(Node node)
+    {
+        Node n1 = node.next;
+        Node n2 = node.prev;
+        n1.next= n2;
+        n2.prev=n1;
+    }
+
+    public int get(int key ){
+        if(!storage.containsKey(key))
+        {
+            System.out.println("Cache Miss");
+            return 0;
+        }
+            Node n1 = storage.get(key);
+            int value = n1.value;
+
+            deleteNode(n1);
+
+            insertAtHead(n1);
+            return value;
+    }
 
 
     public void put(int key,int value)
